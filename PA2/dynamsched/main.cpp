0329b272ac5982b6aaaeda823acd02e5a22954f5
Criley71 <connorriley@outlook.com>
2025-11-02 22:10:28 -0500

    pa2 instructions decoded

--- PA2/dynamsched/main.cpp
+++ PA2/dynamsched/main.cpp
@@ -1,5 +1,7 @@
 #include "config.h"
 #include <iostream>
+#include <map>
+#include <queue>
 #include <regex>
 #include <string>
 #include <vector>
@@ -17,33 +19,64 @@ enum Ops {
   FADD, // 8
   FSUB, // 9
   FMUL, // 10
-  FDIV  // 11
+  FDIV, // 11
+  NULL_OP
 };
 
 class Instruction {
 public:
   Ops op;
-  string rd;
-  string rs1;
-  string rs2;
+  int rd;
+  int rs1;
+  int rs2;
   int address;
   bool is_branch = false;
-  string branch_target;
-  Instruction(Ops op, string rd, string rs1, string rs2, int address, string branch_target) {
-    this->op = op;
-    this->rd = rd;
-    this->rs1 = rs1;
-    this->rs2 = rs2;
-    this->address = address;
-    this->branch_target = branch_target;
-    if (branch_target != "") {
-      this->is_branch = true;
-    }
+  int latency;
+
+  Instruction(Ops op_, int rd_, int rs1_, int rs2_, int addr, bool is_branch_, int lat) {
+    op = op_;
+    rd = rd_;
+    rs1 = rs1_;
+    rs2 = rs2_;
+    address = addr;
+    latency = lat;
+    is_branch = is_branch_;
   }
 };
 
+
+class RegisterAliasTable{
+  public:
+  int rat_id = -1;
+  int val = -1;
+  
+};
+
+class RATs{
+  RegisterAliasTable int_rats[32];
+  RegisterAliasTable fp_rats[32];
+};
+
+class ReservationStations {
+  int eff_addr_size;
+  int fp_add_size;
+  int fp_mul_size;
+  int int_size;
+  int reorder_size;
+
+  int eff_addr_in_use;
+  int fp_add_in_use;
+  int fp_mul_in_use;
+  int int_in_use;
+  int reorder_in_use;
+};
+
+
+
+
 void dynam_schedule(Config config);
-vector<Instruction> get_instructions(Config config); //DONT NEED TO PASS CONFIG
+queue<Instruction> get_instructions(Config config);
+int register_parser(string reg);
 void issue(Config config);
 void execute(Config config);
 void write_result(Config config);
@@ -51,30 +84,37 @@ void write_result(Config config);
 int main() {
   Config config = Config();
   config.print_config();
-  // vector<Instruction> instructions = get_instructions(config);
-  // cout << "\n\n";
-  // for (int i = 0; i < instructions.size(); i++) {
-  //   cout << "OP:    " << instructions[i].op << "\n";
-  //   cout << "rd:    " << instructions[i].rd << "\n";
-  //   cout << "rs1:   " << instructions[i].rs1 << "\n";
-  //   cout << "rs2:   " << instructions[i].rs2 << "\n";
-  //   cout << "addr:  " << instructions[i].address << "\n";
-  //   cout << "btar:  " << instructions[i].branch_target << "\n\n";
-  // }
+  queue<Instruction> instructions = get_instructions(config);
+  cout << "\n\n";
+  while (!instructions.empty()) {
+
+    cout << "OP:    " << instructions.front().op << "\n";
+    cout << "rd:    " << instructions.front().rd << "\n";
+    cout << "rs1:   " << instructions.front().rs1 << "\n";
+    cout << "rs2:   " << instructions.front().rs2 << "\n";
+    cout << "addr:  " << instructions.front().address << "\n";
+    cout << "btar:  " << instructions.front().is_branch << "\n\n";
+    instructions.pop();
+  }
+
   dynam_schedule(config);
 }
 
-vector<Instruction> get_instructions(Config config) {
+queue<Instruction> get_instructions(Config config) {
   string instruction;
-  vector<Instruction> instruction_list;
+  queue<Instruction> instruction_list;
   string rd;
   string rs1;
   string rs2;
+
   while (getline(cin, instruction)) {
+    if (instruction.empty()) {
+      continue;
+    }
     cout << instruction << "\n";
 
     regex delimiters("[ ,():\\t]+");
-    vector<string> pi = {}; // pi = parsed instructions
+    vector<string> pi = {}; // pi = parsed instructions, but also not all the way parsed
     sregex_token_iterator it(instruction.begin(), instruction.end(), delimiters, -1);
     sregex_token_iterator end;
     while (it != end) {
@@ -87,31 +127,76 @@ vector<Instruction> get_instructions(Config config) {
     // LOAD INSTRUCTION PARSED VECTOR FORMAT:       [op, rd, 34, x1, address] (34 and x1 dont matter, they are just memory addresses so no hazards)
     // STORE INSTRUCTION PARSED VECTOR FORMAT:      [op, rs2, 37, x1, address] (37 and x1 dont matter, they are just memory addresses so no hazards)
     // BRANCH INSTRUCTION PARSED VECTOR FORMAT:     [op, rs1, rs2, branch_target]
-    if (pi[0] == "lw") {
-      instruction_list.push_back(Instruction(LW, pi[1], "", "", stoi(pi[4], 0, 10), ""));
-    } else if (pi[0] == "flw") {
-      instruction_list.push_back(Instruction(FLW, pi[1], "", "", stoi(pi[4], 0, 10), ""));
-    } else if (pi[0] == "sw") {
-      instruction_list.push_back(Instruction(SW, "", "", pi[1], stoi(pi[4], 0, 10), ""));
-    } else if (pi[0] == "fsw") {
-      instruction_list.push_back(Instruction(FSW, "", "", pi[1], stoi(pi[4], 0, 10), ""));
-    } else if (pi[0] == "add") {
-      instruction_list.push_back(Instruction(ADD, pi[1], pi[2], pi[3], -1, ""));
-    } else if (pi[0] == "sub") {
-      instruction_list.push_back(Instruction(SUB, pi[1], pi[2], pi[3], -1, ""));
-    } else if (pi[0] == "beq") {
-      instruction_list.push_back(Instruction(BEQ, "", pi[1], pi[2], -1, pi[3]));
-    } else if (pi[0] == "bne") {
-      instruction_list.push_back(Instruction(BNE, "", pi[1], pi[2], -1, pi[3]));
-    } else if (pi[0] == "fadd.s") {
-      instruction_list.push_back(Instruction(FADD, pi[1], pi[2], pi[3], -1, ""));
-    } else if (pi[0] == "fsub.s") {
-      instruction_list.push_back(Instruction(FSUB, pi[1], pi[2], pi[3], -1, ""));
-    } else if (pi[0] == "fmul.s") {
-      instruction_list.push_back(Instruction(FMUL, pi[1], pi[2], pi[3], -1, ""));
-    } else if (pi[0] == "fdiv.s") {
-      instruction_list.push_back(Instruction(FDIV, pi[1], pi[2], pi[3], -1, ""));
+    map<string, Ops> op_map_translator = {
+        {"lw", LW},
+        {"flw", FLW},
+        {"sw", SW},
+        {"fsw", FSW},
+        {"add", ADD},
+        {"sub", SUB},
+        {"beq", BEQ},
+        {"bne", BNE},
+        {"fadd.s", FADD},
+        {"fsub.s", FSUB},
+        {"fmul.s", FMUL},
+        {"fdiv.s", FDIV}};
+    Ops op = op_map_translator[pi[0]];
+    int rd = -1;
+    int rs1 = -1;
+    int rs2 = -1;
+    int address = -1;
+    bool is_branch = false;
+    int latency = 1;
+
+    switch (op) {
+    case LW:
+    case FLW:
+      rd = register_parser(pi[1]);
+      address = stoi(pi[4]);
+      break;
+    case SW:
+    case FSW:
+      rs2 = register_parser(pi[1]);
+      address = stoi(pi[4]);
+      break;
+    case ADD:
+    case SUB:
+    case FADD:
+    case FSUB:
+    case FMUL:
+    case FDIV:
+      rd = register_parser(pi[1]);
+      rs1 = register_parser(pi[2]);
+      rs2 = register_parser(pi[3]);
+      switch (op) {
+      case FADD:
+        latency = config.fp_add_latency;
+        break;
+      case FSUB:
+        latency = config.fp_sub_latency;
+        break;
+      case FMUL:
+        latency = config.fp_mul_latency;
+        break;
+      case FDIV:
+        latency = config.fp_div_latency;
+        break;
+      default:
+        latency = 1;
+        break;
+      }
+      break;
+    case BEQ:
+    case BNE:
+      is_branch = true;
+      rs1 = register_parser(pi[1]);
+      rs2 = register_parser(pi[2]);
+      rd = register_parser(pi[3]);
+      break;
+    default:
+      break;
     }
+    instruction_list.push(Instruction(op, rd, rs1, rs2, address, is_branch, latency));
   }
   return instruction_list;
 }
@@ -122,5 +207,18 @@ void dynam_schedule(Config config) {
   cout << "                                      Memory Writes\n";
   cout << "     Instruction      Issues Executes  Read  Result Commits\n";
   cout << "--------------------- ------ -------- ------ ------ -------\n";
-  get_instructions(config);
+  queue<Instruction> instructions = get_instructions(config);
 }
+
+int register_parser(string reg) {
+  if (reg.empty()) return -1;
+  return stoi(reg.substr(1));
+}
+
+
+/*
+TODO:
+figure out speculative differences from non-speculative
+issue will check if a reservation station is available, if so, it will allocate it and update the RAT
+read jantz notes on speculation
+*/
